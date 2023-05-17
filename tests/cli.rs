fead58b703f0492e4d642f02a890fabfb8bb54f5
olliecheng <contact@olliecheng.me>
2023-05-17 16:56:05 +1000

    Added tests

---
+++ tests/cli.rs
@@ -0,0 +1,28 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+const BINARY: &str = "proj";
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+#[test]
+fn file_doesnt_exist() -> TestResult {
+    let mut cmd = Command::cargo_bin(BINARY)?;
+
+    cmd.arg("file_which_does_not_exist.fastq");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
+
+#[test]
+fn small_file_output() -> TestResult {
+    let mut cmd = Command::cargo_bin(BINARY)?;
+    let intended_out = std::fs::read("data/small.fastq.out")?;
+    cmd.arg("data/small.fastq");
+    cmd.assert().success().stdout(predicate::eq(intended_out));
+
+    Ok(())
+}
