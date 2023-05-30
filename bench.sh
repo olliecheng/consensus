ff9c26470cb721ff35ed653e51d0a6ddd8bac5bd
denosawr <contact@olliecheng.me>
2023-05-30 10:55:12 +1000

    Move temporary files to temp/, add to ignore

--- bench.sh
+++
@@ -1,5 +0,0 @@
-#/bin/zsh
-
-cargo build --profile=release-with-debug
-# /usr/bin/time -h -p leaks -atExit -- target/release-with-debug/proj $@
-/usr/bin/time -h -p -l target/release-with-debug/proj $@ >/dev/null
