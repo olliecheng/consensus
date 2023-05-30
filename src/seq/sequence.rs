60ec244d2777e1cd726694dd6893ac92c2aefe32
denosawr <contact@olliecheng.me>
2023-05-30 16:07:36 +1000

    Add preliminary unit testing

--- src/seq/sequence.rs
+++ src/seq/sequence.rs
@@ -22,14 +22,14 @@ impl Seq {
         self.0.push(dna::a_to_b(b))
     }
 
-    pub fn add_iter(&mut self, i: impl Iterator<Item = u8>) {
+    pub fn push_iter(&mut self, i: impl Iterator<Item = u8>) {
         i.map(dna::a_to_b).for_each(|x| self.0.push(x));
     }
 
-    pub fn from_string(s: String) -> Self {
+    pub fn from_string(s: &str) -> Self {
         let length = s.len();
         let mut seq = Self::with_capacity(length);
-        seq.add_iter(s.bytes());
+        seq.push_iter(s.bytes());
         seq
     }
 }
@@ -43,5 +43,65 @@ impl fmt::Display for Seq {
             .collect::<Vec<&str>>()
             .join("");
         write!(f, "{}", s)
+    }
+}
+#[cfg(test)]
+mod tests {
+    use crate::seq::Seq;
+
+    #[test]
+    fn empty_equality() {
+        let s1 = Seq::with_capacity(100);
+        let s2 = Seq::with_capacity(2);
+        assert_eq!(s1, s2)
+    }
+
+    #[test]
+    fn check_equal() {
+        let s1 = Seq::from_string("ATCGABC");
+        let s2 = Seq::from_string("ATCGABC");
+        assert_eq!(s1, s2)
+    }
+
+    #[test]
+    fn check_notequal() {
+        let s1 = Seq::from_string("ATCDABC");
+        let s2 = Seq::from_string("ATCGABC");
+        assert_ne!(s1, s2)
+    }
+
+    #[test]
+    fn push() {
+        let s1 = Seq::from_string("ATCG");
+        let mut s2 = Seq::new();
+        s2.push(b'A');
+        s2.push(b'T');
+        s2.push(b'C');
+        s2.push(b'G');
+
+        assert_eq!(s1, s2)
+    }
+
+    #[test]
+    fn push_iter() {
+        let s1 = Seq::from_string("ATCGCTA");
+        let mut s2 = Seq::new();
+        s2.push_iter("ATCGCTA".bytes());
+        assert_eq!(s1, s2)
+    }
+
+    #[test]
+    fn format() {
+        let orig_seq = "ATCGACTC".to_string();
+        let s = Seq::from_string(&orig_seq);
+        assert_eq!(s.to_string(), orig_seq);
+        assert_eq!(format!("{}", s), orig_seq)
+    }
+
+    #[test]
+    fn test_bit_order() {
+        let s1 = Seq::from_string("ATCG");
+        let s2 = Seq::from_string("GCTA");
+        assert_ne!(s1, s2);
     }
 }
