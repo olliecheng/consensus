941b855593d7083a4c72ed340fbe9dd8105e7371
olliecheng <olliecheng@outlook.com>
2024-06-25 12:33:45 +1000

    Add generate_index executable to produce a flexiplex_reads_barcodes index file.

---
+++ generate_index/src/main.rs
@@ -0,0 +1,84 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use clap::Parser;
+use csv::{Writer, WriterBuilder};
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    #[arg(long, default_value = "index.tsv")]
+    output: String,
+
+    #[arg(long)]
+    file: String,
+}
+
+fn iter_lines<W: std::io::Write>(mut reader: BufReader<File>, mut wtr: Writer<W>) {
+    let mut position: usize = 0;
+    let mut count: usize = 0;
+
+    // write headers
+    wtr.write_record(&[
+        "Read",
+        "CellBarcode",
+        "FlankEditDist",
+        "BarcodeEditDist",
+        "UMI",
+        "Position",
+    ])
+    .unwrap();
+
+    let mut result = String::new();
+    while let Ok(bsize) = reader.read_line(&mut result) {
+        if bsize == 0 {
+            // EOF has been reached
+            break;
+        }
+
+        if count % 4 == 0 {
+            // extract barcode, UMI, and the read ID
+            // format: @TCTGGCTCATTCTCCG_GCAGCGAAGCCC#32b5d571-ad88-4ac7-bc46-f2ff03de65aa_+1of1
+            let i = result.find('_').unwrap();
+            let j = result.find('#').unwrap();
+            let k = result.rfind('_').unwrap();
+
+            let bc = &result[1..i];
+            let umi = &result[(i + 1)..j];
+            let id = &result[(j + 1)..k];
+
+            let actual_pos = position + (position != 0) as usize;
+
+            //println!("{}, {}, {}, {}", bc, umi, id, actual_pos);
+            wtr.write_record(&[id, bc, "?", "?", umi, &actual_pos.to_string()])
+                .unwrap();
+        }
+        count += 1;
+        position += bsize;
+
+        // reset string
+        result.clear();
+    }
+    wtr.flush().unwrap();
+}
+
+pub fn construct_index(infile: &str, outfile: &str) {
+    let f = File::open(infile).expect("File could not be opened");
+    let reader = BufReader::new(f);
+
+    let wtr = WriterBuilder::new()
+        .delimiter(b'\t')
+        .from_path(outfile)
+        .unwrap();
+
+    iter_lines(reader, wtr);
+}
+
+fn main() {
+    let args = Args::parse();
+
+    construct_index(&args.file, &args.output);
+
+    eprintln!("Completed index generation to {}", &args.output);
+}
