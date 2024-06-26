82a8b603a940530b1d027a0b59a9f1817134e869
olliecheng <olliecheng@outlook.com>
2024-06-26 14:18:20 +1000

    Add index file reading and summary statistic generation

---
+++ consensus/src/duplicates.rs
@@ -0,0 +1,77 @@
+use csv::ReaderBuilder;
+use std::collections::HashMap;
+use std::error::Error;
+
+#[derive(Eq, PartialEq, Hash, Debug)]
+pub struct RecordIdentifier {
+    pub bc: String,
+    pub umi: String,
+}
+
+#[derive(Debug)]
+pub struct DuplicateStatistics {
+    pub total_reads: usize,
+    pub duplicate_reads: usize,
+    pub duplicate_ids: usize,
+    pub proportion_duplicate: f64,
+    pub distribution: HashMap<usize, usize>,
+}
+
+pub fn get_duplicates(
+    index: &str,
+) -> Result<(HashMap<RecordIdentifier, Vec<usize>>, DuplicateStatistics), Box<dyn Error>> {
+    let mut map = HashMap::<RecordIdentifier, Vec<usize>>::new();
+    let mut stats = DuplicateStatistics {
+        total_reads: 0,
+        duplicate_reads: 0,
+        duplicate_ids: 0,
+        proportion_duplicate: 0.0,
+        distribution: HashMap::new(),
+    };
+
+    let mut reader = ReaderBuilder::new()
+        .delimiter(b'\t')
+        .has_headers(true)
+        .from_path(index)?;
+
+    for read in reader.records() {
+        let record = read?;
+        stats.total_reads += 1;
+
+        let id = RecordIdentifier {
+            bc: record[1].to_string(),
+            umi: record[4].to_string(),
+        };
+
+        let index = record[5].parse()?;
+        if let Some(v) = map.get_mut(&id) {
+            v.push(index);
+        } else {
+            map.insert(id, vec![index]);
+        }
+    }
+
+    map.retain(|_, v| v.len() > 1);
+    map.shrink_to_fit(); // optimise memory usage
+
+    stats.duplicate_ids = map.len();
+    stats.duplicate_reads = map
+        .iter()
+        .map(|(_, v)| {
+            let length = v.len();
+            if let Some(x) = stats.distribution.get_mut(&length) {
+                *x += 1
+            } else {
+                stats.distribution.insert(length, 1);
+            }
+            length
+        })
+        .sum();
+    stats
+        .distribution
+        .insert(1, stats.total_reads - stats.duplicate_reads);
+
+    stats.proportion_duplicate = stats.duplicate_reads as f64 / stats.total_reads as f64;
+
+    return Ok((map, stats));
+}
---
+++ consensus/src/main.rs
@@ -0,0 +1,54 @@
+use bio::io::fastq;
+use clap::{Parser, Subcommand};
+
+mod duplicates;
+
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+struct Cli {
+    /// the index file
+    #[arg(long)]
+    index: String,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Generate a summary of duplicate statistics from an index file
+    Summary {},
+
+    /// Generate a consensus-called 'cleaned up' file
+    Generate {
+        /// the input .fastq
+        #[arg(long)]
+        input: String,
+
+        /// the output .fastq
+        #[arg(long)]
+        output: String,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Summary {}) => {
+            let (_, statistics) =
+                duplicates::get_duplicates(&cli.index).expect("Could not parse index.");
+
+            println!("{statistics:#?}")
+        }
+        Some(Commands::Generate { input, output }) => {
+            let (duplicates, statistics) =
+                duplicates::get_duplicates(&cli.index).expect("Could not parse index.");
+
+            for (key, val) in duplicates.iter() {
+                // println!("key: {key:?}, val: {val:?}");
+            }
+        }
+        None => {}
+    }
+}
