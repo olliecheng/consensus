34766f45943c92bb6b2148c3ccea1911b81d8e8c
denosawr <olliecheng@outlook.com>
2024-07-10 17:07:35 +1000

    Rewrite parallelised calling to be a bit more elegant & minimise mutex overhead

--- consensus/src/call.rs
+++ consensus/src/call.rs
@@ -1,4 +1,5 @@
 use crate::duplicates::DuplicateMap;
+use crate::duplicates::RecordIdentifier;
 use bio::io::fastq;
 use bio::io::fastq::{FastqRead, Reader};
 use std::collections::HashMap;
@@ -15,6 +16,11 @@ use rayon::{self, max_num_threads};
 use spoa::{self, AlignmentEngine};
 use std::ffi::{CStr, CString};
 
+struct DuplicateRecord {
+    id: RecordIdentifier,
+    records: Vec<fastq::Record>,
+}
+
 pub fn consensus<R: Write + Send>(
     input: &str,
     writer: &Arc<Mutex<R>>,
@@ -28,69 +34,60 @@ pub fn consensus<R: Write + Send>(
         .unwrap();
 
     let mut file = File::open(input)?;
-    let file = Arc::new(Mutex::new(file));
+    //let file = Arc::new(Mutex::new(file));
 
-    duplicates.par_iter().for_each(|(id, positions)| {
-        if positions.len() == 1 {
-            // TODO: do something here later
-            return;
-        }
+    duplicates
+        .into_iter()
+        // first, read from the file (sequential)
+        .map(|(id, positions)| {
+            let mut rec = DuplicateRecord {
+                id,
+                records: Vec::new(),
+            };
+            for pos in positions.iter() {
+                let mut record = fastq::Record::new();
+                file.seek(SeekFrom::Start(*pos as u64))
+                    .expect("Reading from file should not fail!");
 
-        let file = Arc::clone(&file);
-        let mut file_binding = file.lock().unwrap();
-        let mut file = file_binding.deref_mut();
+                let mut reader = fastq::Reader::new(&mut file);
+                reader.read(&mut record).unwrap();
+                rec.records.push(record);
+            }
+            rec
+        })
+        // convert this sequential iterator into a parallel one for consensus calling
+        .par_bridge()
+        .for_each(|recs| {
+            let mut alignment_engine =
+                spoa::AlignmentEngine::new(spoa::AlignmentType::kOV, 5, -4, -8, -6, -10, -4);
+            let mut poa_graph = spoa::Graph::new();
 
-        // these are the SPOA default values with semi-global alignment
-        let mut alignment_engine =
-            spoa::AlignmentEngine::new(spoa::AlignmentType::kOV, 5, -4, -8, -6, -10, -4);
+            for record in recs.records.iter() {
+                let seq = record.seq();
+                let qual = record.qual();
 
-        // Construct graph
-        let mut poa_graph = spoa::Graph::new();
+                let align = alignment_engine.align_from_bytes(seq, &poa_graph);
+                poa_graph.add_alignment_from_bytes(&align, seq, &qual);
+            }
 
-        for pos in positions.iter() {
-            let mut record = fastq::Record::new();
-            file.seek(SeekFrom::Start(*pos as u64))
-                .expect("Reading from file should not fail");
+            let consensus = poa_graph.consensus();
+            let consensus = consensus
+                .to_str()
+                .expect("spoa module should produce valid utf-8");
 
-            let mut reader = fastq::Reader::new(&mut file);
-            reader.read(&mut record).unwrap();
+            let writer = Arc::clone(writer);
+            let mut writer = writer.lock().unwrap();
 
-            // seq and qual should both be null terminated before casting to CStr
-            let seq_null_term = [record.seq(), b"\0"].concat();
-            let qual_null_term = [record.qual(), b"\0"].concat();
-
-            // add record to graph
-            let seq =
-                CStr::from_bytes_with_nul(&seq_null_term).expect("Casting to cstr should not fail");
-            let qual = CStr::from_bytes_with_nul(&qual_null_term)
-                .expect("Casting to cstr should not fail");
-
-            let align = alignment_engine.align(seq, &poa_graph);
-            poa_graph.add_alignment(&align, seq, &qual);
-
-            // TODO: if asked, write each read as well
-        }
-
-        // free the lock
-        drop(file);
-
-        let cons = poa_graph.consensus();
-        let consensus_seq = cons
-            .to_str()
-            .expect("spoa module should produce valid utf-8");
-
-        let writer = Arc::clone(writer);
-        let mut writer = writer.lock().unwrap();
-
-        writeln!(
-            writer,
-            ">{0}_{1}_CON_{2}\n{3}",
-            id.bc,
-            id.umi,
-            positions.len(),
-            consensus_seq
-        );
-    });
+            writeln!(
+                writer,
+                ">{0}_{1}_CON_{2}\n{3}",
+                recs.id.bc,
+                recs.id.umi,
+                recs.records.len(),
+                consensus
+            )
+            .unwrap();
+        });
 
     return Ok(());
 }
